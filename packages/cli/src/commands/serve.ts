9dd56d7188caaf821e8e2260c2a57e78ca080e6c
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-30 13:49:17 +0200

    start error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,11 +2,34 @@ import path from "path"
 import { Command } from "commander";
 import { serve } from "local-api"
 
+interface LocalApiError {
+  code: string;
+}
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run serve on", "4005")
-  .action((filename: string = "notes.js", options: { port: string })=> {
-    const dir = path.join(process.cwd(), path.dirname(filename))
-    serve(parseInt(options.port), path.basename(filename), dir)
+  .action(async (filename: string = "notes.js", options: { port: string })=> {
+
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
+
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename))
+      await serve(parseInt(options.port), path.basename(filename), dir)
+      console.log(
+        `Opened ${filename}.\n\Navigate to http://localhost:${options.port} to edit the file.`
+      )
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "EADDRINUSE") {
+          console.error(`Port ${options.port} is already in use.\n\Try running on a different port by adding the following to your command: -p <port number>`);
+        }
+      } else if (err instanceof Error) {
+        console.log("Here's the problem", err.message);
+      }
+      process.exit(1)
+    }
   })
