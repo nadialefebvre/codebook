8a76a045bc79c432119c179a773d9b7729262199
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-30 16:32:22 +0200

    resolve path for execution environment

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,12 @@
 import path from "path"
-import { Command } from "commander";
+import { Command } from "commander"
 import { serve } from "local-api"
 
 interface LocalApiError {
-  code: string;
+  code: string
 }
+
+const isProduction = process.env.NODE_ENV === "production"
 
 export const serveCommand = new Command()
   .command("serve [filename]")
@@ -13,22 +15,27 @@ export const serveCommand = new Command()
   .action(async (filename: string = "notes.js", options: { port: string })=> {
 
     const isLocalApiError = (err: any): err is LocalApiError => {
-      return typeof err.code === "string";
-    };
+      return typeof err.code === "string"
+    }
 
     try {
       const dir = path.join(process.cwd(), path.dirname(filename))
-      await serve(parseInt(options.port), path.basename(filename), dir)
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      )
       console.log(
         `Opened ${filename}.\n\Navigate to http://localhost:${options.port} to edit the file.`
       )
     } catch (err) {
       if (isLocalApiError(err)) {
         if (err.code === "EADDRINUSE") {
-          console.error(`Port ${options.port} is already in use.\n\Try running on a different port by adding the following to your command: -p <port number>`);
+          console.error(`Port ${options.port} is already in use.\n\Try running on a different port by adding the following to your command: -p <port number>`)
         }
       } else if (err instanceof Error) {
-        console.log("Here's the problem", err.message);
+        console.log("Here's the problem", err.message)
       }
       process.exit(1)
     }
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,15 +1,26 @@
 import express from "express"
 import { createProxyMiddleware } from "http-proxy-middleware"
+import path from "path"
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express()
 
-  app.use(createProxyMiddleware({
-    target: "http://127.0.0.1:3000",
-    ws: true,
-    logLevel: "silent",
-    changeOrigin: true
-  }))
+  if (useProxy) {
+    app.use(createProxyMiddleware({
+      target: "http://127.0.0.1:3000",
+      ws: true,
+      logLevel: "silent",
+      changeOrigin: true
+    }))
+  } else {
+    const packagePath = require.resolve("local-client/build/index.html")
+    app.use(express.static(path.dirname(packagePath)))
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject)
