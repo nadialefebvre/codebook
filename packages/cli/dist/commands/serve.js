54fab701746e539b8376c2ece17c06e9e29f896a
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-30 12:13:29 +0200

    fix filename and dir for commands

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,10 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
+const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
+const local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
-    .command("serve")
+    .command("serve [filename]")
     .description("Open a file for editing")
-    .action(() => {
-    console.log("Getting ready to serve a file");
+    .option("-p, --port <number>", "port to run serve on", "4005")
+    .action((filename = "notes.js", options) => {
+    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
 });
