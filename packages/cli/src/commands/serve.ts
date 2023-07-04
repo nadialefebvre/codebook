5d30aac5c0911db560239b5ff86f94e44b5b9e20
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-07-04 17:04:34 +0200

    change generated file name

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -12,32 +12,34 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run serve on", "4005")
-  .action(async (filename: string = "notes.js", options: { port: string }) => {
-    const isLocalApiError = (err: any): err is LocalApiError => {
-      return typeof err.code === "string"
-    }
-
-    try {
-      const dir = path.join(process.cwd(), path.dirname(filename))
-      await serve(
-        dir,
-        path.basename(filename),
-        parseInt(options.port),
-        !isProduction
-      )
-      console.log(
-        `Opened ${filename}.\n\Navigate to http://localhost:${options.port} to edit the file.`
-      )
-    } catch (err) {
-      if (isLocalApiError(err)) {
-        if (err.code === "EADDRINUSE") {
-          console.error(
-            `Port ${options.port} is already in use.\n\Try running on a different port by adding the following to your command: -p <port number>`
-          )
-        }
-      } else if (err instanceof Error) {
-        console.log("Here's the problem:", err.message)
+  .action(
+    async (filename: string = "logbook.js", options: { port: string }) => {
+      const isLocalApiError = (err: any): err is LocalApiError => {
+        return typeof err.code === "string"
       }
-      process.exit(1)
+
+      try {
+        const dir = path.join(process.cwd(), path.dirname(filename))
+        await serve(
+          dir,
+          path.basename(filename),
+          parseInt(options.port),
+          !isProduction
+        )
+        console.log(
+          `Opened ${filename}.\n\Navigate to http://localhost:${options.port} to edit the file.`
+        )
+      } catch (err) {
+        if (isLocalApiError(err)) {
+          if (err.code === "EADDRINUSE") {
+            console.error(
+              `Port ${options.port} is already in use.\n\Try running on a different port by adding the following to your command: -p <port number>`
+            )
+          }
+        } else if (err instanceof Error) {
+          console.log("Here's the problem:", err.message)
+        }
+        process.exit(1)
+      }
     }
-  })
+  )
